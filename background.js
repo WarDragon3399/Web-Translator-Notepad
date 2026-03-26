5a14838a0700a28c179990a8893a2293c499b3a8
Parthkuamr Rathod <parthrathodtk@gmail.com>
2026-03-26 10:06:48 +0530

    Update background.js

--- background.js
+++ background.js
@@ -5,3 +5,8 @@ chrome.action.onClicked.addListener(() => {
     url: chrome.runtime.getURL("index.html")
   });
 });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "openLensTab") {
+        chrome.tabs.create({ url: "https://lens.google.com/upload?ep=subbar" });
+    }
+});
